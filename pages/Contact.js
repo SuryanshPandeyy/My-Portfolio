f9534fd656b641b8ea94039ec3aa9665793819cd
Suryanshpsurya <65591693+Suryanshpsurya@users.noreply.github.com>
2021-09-21 15:13:28 +0530

    updating app

--- pages/Contact.js
+++ pages/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => {
       message: enteredMessage,
     };
 
-    await fetch("/pages/api/users", {
+    await fetch("api/users", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
