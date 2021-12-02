ace3f3d05511bc7c16dd9f6c0bf6514f8e55b79b
Suryanshpsurya <65591693+Suryanshpsurya@users.noreply.github.com>
2021-12-02 15:20:59 +0530

    added skills

--- public/json/Skills.js
+++ public/json/Skills.js
@@ -1,17 +1,17 @@
 const Skills = [
-  { id: 1, title: "NEXTJS", value: "70" },
+  { id: 1, title: "NEXTJS", value: "90" },
   { id: 2, title: "MongoDB", value: "95" },
-  { id: 3, title: "React", value: "90" },
+  { id: 3, title: "React", value: "95" },
   { id: 4, title: "NodeJS", value: "90" },
-  { id: 5, title: "ExpressJS", value: "70" },
+  { id: 5, title: "ExpressJS", value: "75" },
   { id: 6, title: "Html, CSS, SASS", value: "100" },
   { id: 7, title: "Reacr Native", value: "15" },
   { id: 8, title: "Javascript", value: "95" },
   { id: 9, title: "JQuery", value: "90" },
-  { id: 10, title: "Wordpress", value: "20" },
+  { id: 10, title: "Wordpress", value: "25" },
   { id: 11, title: "AWS", value: "5" },
-  { id: 12, title: "UI", value: "5" },
-  { id: 13, title: "Php", value: "90" },
+  { id: 12, title: "UI", value: "10" },
+  { id: 13, title: "Php", value: "98" },
   { id: 14, title: "MySQL", value: "100" },
 ];
 
