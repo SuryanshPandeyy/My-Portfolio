fd7764cfc432b75070f8832f1f9058650a02f054
Suryansh Pandey <suryanshpsurya@gmail.com>
2023-09-13 12:35:46 +0530

    Update Skills.js

--- public/json/Skills.js
+++ public/json/Skills.js
@@ -1,25 +1,20 @@
 const Skills = [
-  { title: "C language", value: "80" },
-  { title: "C++", value: "40" },
   { title: "App Development", value: "90" },
   { title: "Web Development", value: "95" },
   { title: "Web Designing", value: "99" },
-  { title: "React Native", value: "90" },
-  { title: "ElectronJs", value: "60" },
+  { title: "React Native", value: "100" },
   { title: "Figma", value: "95" },
   { title: "NEXTJS", value: "100" },
-  { title: "NextronJs", value: "40" },
-  { title: "MongoDB", value: "95" },
-  { title: "React", value: "95" },
-  { title: "NodeJS", value: "90" },
-  { title: "ExpressJS", value: "85" },
+  { title: "MongoDB", value: "100" },
+  { title: "React", value: "100" },
+  { title: "NodeJS", value: "95" },
+  { title: "ExpressJS", value: "95" },
   { title: "Html, CSS, SASS", value: "100" },
-  { title: "Javascript", value: "95" },
-  { title: "JQuery", value: "70" },
-  { title: "Wordpress", value: "75" },
-  { title: "AWS", value: "30" },
+  { title: "Javascript", value: "100" },
+  { title: "JQuery", value: "85" },
   { title: "Php Core", value: "90" },
   { title: "MySQL", value: "90" },
+  {title: "Python Intermediate", value: "80"}
 ];
 
 export default Skills;
